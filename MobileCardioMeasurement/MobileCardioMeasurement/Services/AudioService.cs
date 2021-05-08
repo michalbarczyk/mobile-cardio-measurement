fd4b99a52e12c65f76809f264cd8c77dec34a9ed
Michał <barczykmt@gmail.com>
2021-05-09 01:01:48 +0200

    add sound recording and playing

---
+++ MobileCardioMeasurement/MobileCardioMeasurement/Services/AudioService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Plugin.AudioRecorder;
+
+namespace MobileCardioMeasurement.Services
+{
+	public class AudioService
+	{
+		private AudioRecorderService _audioRecorder;
+		private AudioPlayer _audioPlayer;
+		
+		public AudioService()
+		{
+			_audioRecorder = new AudioRecorderService
+			{
+				StopRecordingAfterTimeout = false,
+				TotalAudioTimeout = TimeSpan.FromSeconds(15),
+				AudioSilenceTimeout = TimeSpan.FromSeconds(2)
+			};
+			_audioPlayer = new AudioPlayer();
+		}
+		
+		public async Task StartRecording()
+        {
+            if (!_audioRecorder.IsRecording)
+            {
+	            await await _audioRecorder.StartRecording();
+            }  
+        }
+           
+        public async Task StopRecording()
+        {
+	        await _audioRecorder.StopRecording();
+        }
+
+        public void Play()
+        {
+	        var filePath = _audioRecorder.GetAudioFilePath();
+	        if (filePath != null)
+	        {
+		        _audioPlayer.Play(filePath);
+	        }
+        }
+	}
+}
--- MobileCardioMeasurement/MobileCardioMeasurement/ViewModels/MainViewModel.cs
+++ MobileCardioMeasurement/MobileCardioMeasurement/ViewModels/MainViewModel.cs
@@ -1,7 +1,21 @@
-﻿namespace MobileCardioMeasurement.ViewModels
+﻿using MobileCardioMeasurement.Services;
+using Xamarin.Forms;
+
+namespace MobileCardioMeasurement.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
+        private readonly AudioService _audioService;
+        public MainViewModel()
+        {
+            _audioService = new AudioService();
+            StartRecordingCommand = new Command(async () => await _audioService.StartRecording());
+            StopRecordingCommand = new Command(async () => await _audioService.StopRecording());
+            PlayCommand = new Command(() => _audioService.Play());
+        }
         
+        public Command StartRecordingCommand { get; }
+        public Command StopRecordingCommand { get; }
+        public Command PlayCommand { get; }
     }
 }
