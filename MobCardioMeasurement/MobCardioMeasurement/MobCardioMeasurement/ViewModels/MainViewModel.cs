bafb3f66265e212b5c5fbc2e7a7d4ed8b01d7e26
asmoter <a.m.smoter@gmail.com>
2021-06-01 22:04:19 +0200

    parsing wav

--- MobCardioMeasurement/MobCardioMeasurement/MobCardioMeasurement/ViewModels/MainViewModel.cs
+++ MobCardioMeasurement/MobCardioMeasurement/MobCardioMeasurement/ViewModels/MainViewModel.cs
@@ -2,9 +2,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Xamarin.Forms;
 using MobCardioMeasurement.ViewModels.Base;
 using System.Threading.Tasks;
+using Accord.Audio;
 
 namespace MobCardioMeasurement.ViewModels
 {
@@ -25,13 +27,49 @@ namespace MobCardioMeasurement.ViewModels
                 IsLoading = true;
                 Path = await _audioService.RecordSample(TimeSpan.FromSeconds(6));
                 IsLoading = false;
-
+                
                 var wav = new WavService(_path);
+                
+                var ecg = wav.Data;
+                var max = ecg.Max();
+                const int frameSize = 1000;
+                var threshold = 0.15;
 
-                Metadata = $"Rate: {wav.SampleRate}\nLength: {wav.Data.Length}";
+                var data = ecg.Select(val => (double) val).ToArray();
+                var peaksAll = data.FindPeaks(); // indeksy peakow
+                var size = peaksAll.Length;
+
+                var dataMovingAvg = movingAvg(ecg, frameSize);
+                var peaksMovingAvg = dataMovingAvg.FindPeaks();
+
+                var rPeaks = peaksMovingAvg.Where(peak => dataMovingAvg[peak] > threshold).Select(peak => (short) peak).ToList();
+                var rPeaksSize = rPeaks.Count;
+
+                Metadata = $"Rate: {wav.SampleRate}\nLength: {wav.Data.Length}\nR peaks: {rPeaksSize}\n";
 
                 DataCalculated?.Invoke(wav.Data);
             });
+        }
+        
+        public double[] movingAvg(Int16[] data, int frameSize)
+        {
+            double sum = 0;
+            double[] avgPeaks = new double[data.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                int counter = 0;
+                int index = counter;
+                while (counter < frameSize)
+                {
+                    sum += data[index];
+                    counter++;
+                    index++;
+                }
+
+                avgPeaks[i] = sum / frameSize;
+                sum = 0;
+            }
+            return avgPeaks;
         }
         
         public bool IsLoading
